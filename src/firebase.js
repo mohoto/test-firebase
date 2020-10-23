816922ede7edade70200eeeeb73287cfc9d857b2
Walid Benmouffok <walid.benmouffok@gmail.com>
2020-10-23 20:06:45 +0200

    deploy 2

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+  import firebase from 'firebase';
+
+  const firebaseApp = firebase.InitializeApp({
+        apiKey: "AIzaSyAR4M-pfj0DG8LEsexNrKHkBycLEBH2unA",
+        authDomain: "app-test-a1a35.firebaseapp.com",
+        databaseURL: "https://app-test-a1a35.firebaseio.com",
+        projectId: "app-test-a1a35",
+        storageBucket: "app-test-a1a35.appspot.com",
+        messagingSenderId: "823628415021",
+        appId: "1:823628415021:web:8543ae286f9047efcf78a4"
+  });
+
+  const db = firebaseApp.firestore();
+
+  export {db};
